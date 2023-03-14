2b216fbf9b8d7f2dc5e66179b6880796523db7fe
amanguptaBay <47506369+amanguptaBay@users.noreply.github.com>
2023-03-14 11:03:03 -0700

    Orchestrated Model
    Using class to orchestrate training and predicting

--- finalProject/main.py
+++ finalProject/main.py
@@ -1,7 +1,7 @@
 from data_cleaning import clean_data, get_user_watched
 import pandas as pd
 
-from finalProject.weighted_prediction import make_prediction
+from finalProject.weighted_prediction import FinalModel
 
 if __name__ == "__main__":
     print("Running Predictions")
@@ -15,12 +15,18 @@ if __name__ == "__main__":
     print("Cleaning Data, and generating intermediate cleaned data")
     df = clean_data(watchings,movies , movie_watchings)
     user_watched = get_user_watched(df)
+
     if SAVE_INTERMEDIATES:
         print("Saving data to intermediates folder")
         df.to_csv("intermediates/df.csv", encoding = "utf-8-sig")
         user_watched.to_csv("intermediates/user_watched.csv", encoding = "utf-8-sig")
+
     uid = 2976
     weights = (80,10,10)
+
+    final = FinalModel(movies, user_watched, df)
+    print("Training model")
+    final.train()
     print(f"Getting prediction for user: {uid}, with weights: {weights} ")
-    print(make_prediction(uid,weights,movies, user_watched, df))
+    print(final.predict(2976, (80, 10, 10)))
 
--- finalProject/weighted_prediction.py
+++ finalProject/weighted_prediction.py
@@ -6,43 +6,63 @@ from finalProject.hotel_trending import output_score
 from finalProject.monthly_reccomendation import monthlyTrendyWrapper
 from finalProject.svdpp import trainSVDPP
 
+class FinalModel():
+    def __init__(self, movies: pd.DataFrame, user_watched:pd.DataFrame, df:pd.DataFrame):
+        assert isinstance(movies, pd.DataFrame)
+        assert isinstance(user_watched, pd.DataFrame)
+        assert isinstance(df, pd.DataFrame)
+        assert "MovieID" in movies.columns
+        self.moviesDF = movies
+        self.user_watchedDF = user_watched
+        self.dfDF = df
 
-def make_prediction(uid: int, weights: tuple[int,int,int], movies: pd.DataFrame, user_watched:pd.DataFrame, df:pd.DataFrame, k = 10):
-    assert isinstance(weights, tuple), "Weights are not a tuple"
-    assert all((isinstance(weight, int) for weight in weights)), "One or more of the weights is not a int"
-    assert all((weight >= 0 for weight in weights)), "One or more of the weights is negative"
-    assert sum(weights) == 100, "Sum of weights is not 100"
-    assert isinstance(uid, int)
-    assert isinstance(movies, pd.DataFrame)
-    assert isinstance(user_watched, pd.DataFrame)
-    assert isinstance(df, pd.DataFrame)
-    assert "MovieID" in movies.columns
+    def train(self):
+        # Get Collaborative Filtering Prediction
+        self.svdpPredictor = trainSVDPP(self.user_watchedDF)
 
-    collaborativeFilteringWeight, monthlyTrendingWeight, hotelTrendingWeight = weights
+        # Get Monthly Trending Prediction
+        monthPrediction = monthlyTrendyWrapper(self.dfDF, datetime.datetime.now().month, bookings=250, sd_diff=1.5,
+                                               weightEqual=False).rename(
+            columns={"ranking": "Monthly_Trending_Prediction"}).set_index("MovieID")
+        monthPrediction["Monthly_Trending_Prediction"] = (monthPrediction["Monthly_Trending_Prediction"] / np.sum(
+            monthPrediction["Monthly_Trending_Prediction"]))
 
-    #Get Collaborative Filtering Prediction
-    svdpp_prediction = trainSVDPP(user_watched)(uid).set_index("MovieID").rename(columns = {"Prediction":"SVDpp_Prediction"})
-    svdpp_prediction["SVDpp_Prediction"] = (svdpp_prediction["SVDpp_Prediction"]/np.sum(svdpp_prediction["SVDpp_Prediction"]))*collaborativeFilteringWeight
+        # Get Hotel Trending Prediction
+        hotelTrending = output_score(self.dfDF).rename(columns={"score": "Hotel_Trending_Prediction"}).set_index("MovieID")
+        hotelTrending["Hotel_Trending_Prediction"] = (
+                    hotelTrending["Hotel_Trending_Prediction"] / np.sum(hotelTrending["Hotel_Trending_Prediction"]))
 
-    #Get Monthly Trending Prediction
-    monthPrediction = monthlyTrendyWrapper(df, datetime.datetime.now().month, bookings=250, sd_diff=1.5, weightEqual=False).rename(columns = {"ranking":"Monthly_Trending_Prediction"}).set_index("MovieID")
-    monthPrediction["Monthly_Trending_Prediction"] = (monthPrediction["Monthly_Trending_Prediction"] / np.sum(
-        monthPrediction["Monthly_Trending_Prediction"])) * monthlyTrendingWeight
+        movies = self.moviesDF[["MovieID"]].set_index("MovieID")
+        self.moviesAndAllPredictions = movies.join(monthPrediction, on="MovieID").join(hotelTrending, on="MovieID").fillna(0)
+    def predict(self, uid: int, weights: tuple[int,int,int], k:int = 10):
+        assert hasattr(self, "moviesAndAllPredictions"), "Did not train before trying to predict"
+        assert isinstance(weights, tuple), "Weights are not a tuple"
+        assert all((isinstance(weight, int) for weight in weights)), "One or more of the weights is not a int"
+        assert all((weight >= 0 for weight in weights)), "One or more of the weights is negative"
+        assert sum(weights) == 100, "Sum of weights is not 100"
+        assert isinstance(uid, int)
 
-    #Get Hotel Trending Prediction
-    hotelTrending = output_score(df).rename(columns = {"score":"Hotel_Trending_Prediction"}).set_index("MovieID")
-    hotelTrending["Hotel_Trending_Prediction"] = (hotelTrending["Hotel_Trending_Prediction"] / np.sum(hotelTrending["Hotel_Trending_Prediction"])) * hotelTrendingWeight
+        #Get Collaborative Filtering Prediction
+        svdpp_prediction = self.svdpPredictor(uid).set_index("MovieID").rename(columns={"Prediction": "SVDpp_Prediction"})
+        svdpp_prediction["SVDpp_Prediction"] = (
+                    svdpp_prediction["SVDpp_Prediction"] / np.sum(svdpp_prediction["SVDpp_Prediction"]))
 
-    movies = movies[["MovieID"]].set_index("MovieID")
-    moviesAndAllPredictions = movies.join(svdpp_prediction, on="MovieID").join(monthPrediction, on="MovieID").join(hotelTrending, on="MovieID").fillna(0)
+        total = self.moviesAndAllPredictions.join(svdpp_prediction, on = "MovieID").fillna(0)
 
-    moviesAndAllPredictions["TotalScore"] = moviesAndAllPredictions["Hotel_Trending_Prediction"] + moviesAndAllPredictions["Monthly_Trending_Prediction"] + moviesAndAllPredictions["SVDpp_Prediction"]
-    moviesAndAllPredictions.sort_values(by = ["TotalScore"], inplace=True, ascending = False)
-    return moviesAndAllPredictions[["TotalScore"]].iloc[:k]
+        collaborativeFilteringWeight, monthlyTrendingWeight, hotelTrendingWeight = weights
 
+        total["TotalScore"] = total["Hotel_Trending_Prediction"]*hotelTrendingWeight + \
+                                                total["Monthly_Trending_Prediction"]*monthlyTrendingWeight + \
+                                                total["SVDpp_Prediction"]*collaborativeFilteringWeight
+        total.sort_values(by=["TotalScore"], inplace=True, ascending=False)
+        return total[["TotalScore"]].iloc[:k]
 
 if __name__ == "__main__":
     df = pd.read_csv("intermediates/df.csv")
     movies = pd.read_csv("../data/movies.csv")
     user_watched = pd.read_csv("intermediates/user_watched.csv")
-    print(make_prediction(2976,(80,10,10),movies, user_watched, df))
+
+    final = FinalModel(movies, user_watched, df)
+    final.train()
+
+    print(final.predict(2976,(80,10,10)))
